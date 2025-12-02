19ffe83d30c9a3a869a7d1f1d0c1c2daf4c5110b
VicenteSK <valenzuelacortes@gmail.com>
2025-12-02 00:36:51 -0300

    extent report login

---
+++ Selenium_TestNG_SauceDemo_Java/src/test/java/listeners/TestListener.java
@@ -0,0 +1,52 @@
+package listeners;
+
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentSparkReporter spark = new ExtentSparkReporter("extent-reports/ExecutionReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(
+                result.getMethod().getMethodName(),
+                result.getMethod().getDescription()
+        );
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().log(Status.FAIL, "Test Failed");
+        test.get().log(Status.FAIL, result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().log(Status.SKIP, "Test Skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
