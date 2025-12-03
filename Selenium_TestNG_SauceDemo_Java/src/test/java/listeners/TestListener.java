51a866366b4de11dcdcdc175f895933a04570787
VicenteSK <valenzuelacortes@gmail.com>
2025-12-03 14:56:40 -0300

    feat: agregar captura de pantalla en onTestFailure y actualizar reporte de pruebas

--- Selenium_TestNG_SauceDemo_Java/src/test/java/listeners/TestListener.java
+++ Selenium_TestNG_SauceDemo_Java/src/test/java/listeners/TestListener.java
@@ -4,9 +4,16 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class TestListener implements ITestListener {
 
@@ -22,12 +29,11 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult result) {
-
         Object[] params = result.getParameters();
         String dynamicName = result.getMethod().getMethodName();
 
         if (params != null && params.length > 0) {
-            dynamicName += " - " + params[0].toString(); // username
+            dynamicName += " - " + params[0].toString();
         }
 
         ExtentTest extentTest = extent.createTest(dynamicName);
@@ -41,8 +47,35 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
+
         test.get().log(Status.FAIL, "Test Failed");
         test.get().log(Status.FAIL, result.getThrowable());
+
+        // Obtener driver desde BaseTest
+        WebDriver driver = (WebDriver) result.getAttribute("driver");
+
+        if (driver != null) {
+            try {
+                // Crear carpeta si no existe
+                File screenshotsFolder = new File("screenshots");
+                if (!screenshotsFolder.exists()) {
+                    screenshotsFolder.mkdir();
+                }
+
+                // Nombre del archivo
+                String screenshotPath = "screenshots/" + result.getName() + ".png";
+
+                // Tomar screenshot
+                File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                Files.copy(src.toPath(), new File(screenshotPath).toPath());
+
+                // Adjuntar al reporte
+                test.get().addScreenCaptureFromPath(screenshotPath);
+
+            } catch (IOException e) {
+                test.get().log(Status.WARNING, "No se pudo guardar el screenshot: " + e.getMessage());
+            }
+        }
     }
 
     @Override
--- Selenium_TestNG_SauceDemo_Java/src/test/java/tests/BaseTest.java
+++ Selenium_TestNG_SauceDemo_Java/src/test/java/tests/BaseTest.java
@@ -13,6 +13,9 @@ public class BaseTest {
     public void setup() {
         driver = DriverManager.createDriver();
         driver.get("https://www.saucedemo.com/");
+
+        // Guardar driver para el listener
+        org.testng.Reporter.getCurrentTestResult().setAttribute("driver", driver);
     }
 
     @AfterMethod
